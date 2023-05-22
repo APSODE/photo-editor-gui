22752ac18c2e6de5b972bb5d4c52d822a40d4ad4
windi159 <wontak12345@gmail.com>
2023-05-23 01:30:59 +0900

    PhotoEditorFunc 추가

---
+++ src/PhotoEditorFunc.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from tkinter import Tk, Canvas, NW
+from PIL import Image, ImageTk
+
+
+class PhotoEditorFunc:
+    def __init__(self, gui_master: Tk):
+        self._master = gui_master
+
+    def _display_photo(self, img: Image, height: int, width: int):
+        canvas = Canvas(
+            master = self._master,
+            width = width,
+            height = height
+        )
+        cImage = ImageTk.PhotoImage(img)
+
+        canvas.create_image(0, 0, anchor = NW, image = cImage)
+        canvas.pack()
+        self._master.mainloop()
