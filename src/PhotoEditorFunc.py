c29d590ab33081f0aa59b00e363d5f56bcdc6151
APSODE <2gunbo74@gmail.com>
2023-05-25 10:29:04 +0900

    전체적인 indent 수정과 잘못된 코드에 주석 추가

--- src/PhotoEditorFunc.py
+++ src/PhotoEditorFunc.py
@@ -17,15 +17,27 @@ class PhotoEditorFunc:
         )
         ChangeImage = ImageTk.PhotoImage(img)
 
+        # cImage가 ChangeImage로 변경되었지만 의존관계에 있는 코드들의 수정이 이루어지지 않았음
         canvas.create_image(0, 0, anchor = NW, image = cImage)
         canvas.pack()
 
     def _open_file(self):
-        FileName = askopenfilename(
+        file_name = askopenfilename(
             parent = self._master,
-            filetypes= (("모든 그림 파일", "*.jpg; *.jpeg; *.bmp; *.png; *.tif"), ("모든 파일", " *.* "))
-                                   )
-        InPhoto = Image.opne(filename)
+            filetypes = (
+                (
+                    "모든 그림 파일",
+                    "*.jpg; *.jpeg; *.bmp; *.png; *.tif"
+                ),
+                (
+                    "모든 파일",
+                    " *.* "
+                )
+            )
+        )
+        InPhoto = Image.open(file_name)
+
+        # InX와 InY는 해당 메소드에서 사용하지 않지만 변수가 생성됨.
         InX = InPhoto.width
         InY = InPhoto.height
 
@@ -35,14 +47,26 @@ class PhotoEditorFunc:
         self._display_photo(OutPhoto, OutY, OutX)
 
     def saveimagefile(self):
+        # PhotoEditorFunc.get_OutPhoto 메소드가 정의되지 않았는데 사용함.
         SavePhoto = PhotoEditorFunc.get_OutPhoto()
+
         if SavePhoto is None:
             return
+
         SaveFile = asksaveasfile(
-            parent=self._master,
-            mode="w",
-            defaultextension=".jpg",
-            filetypes=(("JPG파일", "*.jpg; *jpeg"), ("모든 파일", "*.*"))
+            parent = self._master,
+            mode = "w",
+            defaultextension = ".jpg",
+            filetypes = (
+                (
+                    "JPG파일",
+                    "*.jpg; *jpeg"
+                ),
+                (
+                    "모든 파일",
+                    "*.*"
+                )
+            )
         )
         SavePhoto.save(SaveFile.name)
     
@@ -50,9 +74,15 @@ class PhotoEditorFunc:
         scale = askinteger(
             "확대배수",
             "확대할 배수를 입력하세요",
-            minvalue=2,
-            maxvalue=8
+            minvalue = 2,
+            maxvalue = 8
         )
+
+        # PhotoEditorFunc.get_OutPhoto 메소드가 정의되지 않았는데 사용함.
         ShowImage = PhotoEditorFunc.get_OutPhoto()
+
+        # PhotoEditorFunc의 멤버변수로 InX, InY는 존재하지 않음.
+        # PhotoEditorFunc에 존재하지 않는 메소드에 접근함.
+        # InX, InY, scale 전부 int이지만 int 형변환을 사용함. -> 형변환 필요 없음
         ShowImage = ShowImage.resize((int(PhotoEditorFunc.get_InX * scale), int(PhotoEditorFunc.get(InY * scale))))
         ShowImage
