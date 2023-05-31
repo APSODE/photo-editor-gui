5335bb37c4427f21eba7bc0ad715b646430b8460
Windi159 <107599426+Windi159@users.noreply.github.com>
2023-05-31 13:35:23 +0900

    Update PhotoEditorFunc.py
    주석 부분 수정, 기본 매뉴 구현 해놓음

--- src/PhotoEditorFunc.py
+++ src/PhotoEditorFunc.py
@@ -2,29 +2,31 @@ from typing import Optional
 from tkinter.filedialog import askopenfilename, asksaveasfile
 from tkinter.simpledialog import askinteger
 from tkinter import Tk, Canvas, NW
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageEnhance, ImageFilter
 
 
 class PhotoEditorFunc:
+    OutPhoto = None
+
     def __init__(self, gui_master: Tk):
         self._master = gui_master
 
-    def _display_photo(self, img: Image, height: int, width: int):
+    def _display_photo(self, height: int, width: int):
         canvas = Canvas(
-            master = self._master,
-            width = width,
-            height = height
+            master=self._master,
+            width=width,
+            height=height
         )
-        ChangeImage = ImageTk.PhotoImage(img)
+        ChangeImage = ImageTk.PhotoImage(OutPhoto)
 
         # cImage가 ChangeImage로 변경되었지만 의존관계에 있는 코드들의 수정이 이루어지지 않았음
-        canvas.create_image(0, 0, anchor = NW, image = cImage)
+        canvas.create_image(0, 0, anchor=NW, image=ChangeImage)
         canvas.pack()
 
     def _open_file(self):
         file_name = askopenfilename(
-            parent = self._master,
-            filetypes = (
+            parent=self._master,
+            filetypes=(
                 (
                     "모든 그림 파일",
                     "*.jpg; *.jpeg; *.bmp; *.png; *.tif"
@@ -35,29 +37,24 @@ class PhotoEditorFunc:
                 )
             )
         )
-        InPhoto = Image.open(file_name)
 
-        # InX와 InY는 해당 메소드에서 사용하지 않지만 변수가 생성됨.
-        InX = InPhoto.width
-        InY = InPhoto.height
-
-        OutPhoto = InPhoto.copy()
+        OutPhoto = Image.open(file_name)
         OutX = OutPhoto.width
         OutY = OutPhoto.height
-        self._display_photo(OutPhoto, OutY, OutX)
+        self._display_photo(OutY, OutX)
 
     def saveimagefile(self):
         # PhotoEditorFunc.get_OutPhoto 메소드가 정의되지 않았는데 사용함.
-        SavePhoto = PhotoEditorFunc.get_OutPhoto()
+        SavePhoto = OutPhoto
 
         if SavePhoto is None:
             return
 
         SaveFile = asksaveasfile(
-            parent = self._master,
-            mode = "w",
-            defaultextension = ".jpg",
-            filetypes = (
+            parent=self._master,
+            mode="w",
+            defaultextension=".jpg",
+            filetypes=(
                 (
                     "JPG파일",
                     "*.jpg; *jpeg"
@@ -69,20 +66,73 @@ class PhotoEditorFunc:
             )
         )
         SavePhoto.save(SaveFile.name)
-    
+
     def zoomin(self):
-        scale = askinteger(
+        ZoomInScale = askinteger(
             "확대배수",
             "확대할 배수를 입력하세요",
-            minvalue = 2,
-            maxvalue = 8
+            minvalue=2,
+            maxvalue=8
         )
 
-        # PhotoEditorFunc.get_OutPhoto 메소드가 정의되지 않았는데 사용함.
-        ShowImage = PhotoEditorFunc.get_OutPhoto()
 
-        # PhotoEditorFunc의 멤버변수로 InX, InY는 존재하지 않음.
-        # PhotoEditorFunc에 존재하지 않는 메소드에 접근함.
-        # InX, InY, scale 전부 int이지만 int 형변환을 사용함. -> 형변환 필요 없음
-        ShowImage = ShowImage.resize((int(PhotoEditorFunc.get_InX * scale), int(PhotoEditorFunc.get(InY * scale))))
-        ShowImage
+        OutPhoto = OutPhoto.resize((OutPhoto.height * ZoomInScale, OutPhoto.width * ZoomInScale))
+        self._display_photo(OutPhoto.height, OutPhoto.width)
+
+    def zoomout(self):
+        ZoomOutScale = askinteger(
+            "확대배수",
+            "확대할 배수를 입력하세요",
+            minvalue=2,
+            maxvalue=8
+        )
+
+        OutPhoto = OutPhoto.resize((OutPhoto.height / ZoomOutScale, OutPhoto.width / ZoomOutScale))
+        self._display_photo(OutPhoto.height, OutPhoto.width)
+
+    def upsidedown(self):
+        OutPhoto = OutPhoto.transpose(Image.FLIP_TOP_BOTTOM)
+        self._display_photo(OutPhoto.height, OutPhoto.width)
+
+    def leftright(self):
+        OutPhoto = OutPhoto.transpose(Image.FLIP_LEFT_RIGHT)
+        self._display_photo(OutPhoto.height, OutPhoto.width)
+
+    def rotate(self):
+        angle = askinteger(
+            "회전",
+            "회전할 각도를 입력하세요",
+            minvalue=0,
+            maxvalue=360
+        )
+
+        OutPhoto = OutPhoto.rotate(angle, expand=True)
+        self._display_photo(OutPhoto.height, OutPhoto.width)
+
+    def bright(self):
+        degree = askfloat(
+            "밝게/어둡게",
+            "값을 입력하세요(0.0 ~ 5.0)",
+            minvalue=0.0,
+            maxvalue=5.0
+        )
+
+        OutPhoto = ImageEnhance.Brightness(OutPhoto).enhance(degree)
+        self._display_photo(OutPhoto.height, OutPhoto.width)
+
+    def embos(self):
+        OutPhoto = OutPhoto.filter(ImageFilter.EMBOSS)
+        self._display_photo(OutPhoto.height, OutPhoto.width)
+
+    def blur(self):
+        OutPhoto = OutPhoto.filter(ImageFilter.BLUR)
+        self._display_photo(OutPhoto.height, OutPhoto.width)
+
+    def sketch(self):
+        OutPhoto = OutPhoto.filter(ImageFilter.CONTOUR)
+        self._display_photo(OutPhoto.height, OutPhoto.width)
+
+    def edge(self):
+        OutPhoto = OutPhoto.filter(ImageFilter.FIND_EDGES)
+        self._display_photo(OutPhoto.height, OutPhoto.width)
+
